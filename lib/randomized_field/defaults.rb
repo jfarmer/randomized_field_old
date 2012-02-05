75694f3972800fd857d3cac53d3776e73e4c141b
Jesse Farmer <jesse@20bits.com>
2012-02-04 19:18:31 -0800

    Include 0 in the list of default characters

--- lib/randomized_field/defaults.rb
+++ lib/randomized_field/defaults.rb
@@ -2,7 +2,7 @@ module RandomizedField
   # The default values for randomized_field
   DEFAULTS = {
     :length           => 8,
-    :valid_characters => (1..9).to_a + ('a'..'z').to_a,
+    :valid_characters => (0..9).to_a + ('a'..'z').to_a,
     :prefix           => ''
   }
 end
